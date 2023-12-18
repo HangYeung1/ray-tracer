0ea028f971654a458a8c985d87277e3bcb002ab9
Hang Yeung <yeungh@umich.edu>
2023-12-18 10:04:14 -0500

    Combine Color .cpp/.hpp

--- Color.hpp
+++ Color.hpp
@@ -11,6 +11,10 @@ using Color = Vector3d;
 // MODIFIES: os
 // EFFECTS:  Print color converted to 8 bit numbers 
 // EXAMPLE:  Vector3d(1, 1, 1) -> "255 255 255"
-void write_color(std::ostream &os, const Color &color);
+void write_color(std::ostream &os, const Color &color) {
+  os << static_cast<int>(255.999 * color.x()) << ' '
+     << static_cast<int>(255.999 * color.y()) << ' '
+     << static_cast<int>(255.999 * color.z()) << '\n';
+}
 
 #endif
