d31dfa4feeb2065178b88c782be0b5460afad448
Hang Yeung <yeungh@umich.edu>
2023-12-18 12:15:13 -0500

    Fix Interval .cpp/.hpp seperation

--- Interval.cpp
+++ Interval.cpp
@@ -1,5 +1,34 @@
 #include "Interval.hpp"
 
+// EFFECTS:  Initialize Interval to empty aka (inf, -inf)
+Interval::Interval()
+  : minimum(std::numeric_limits<double>::infinity()),
+    maximum(-std::numeric_limits<double>::infinity()) { }
+
+// EFFECTS:  Initialize Interval to minimum_in, max_in
+Interval::Interval(const double minimum_in, const double maximum_in)
+  : minimum(minimum_in), maximum(maximum_in) { }
+
+// EFFECTS:  Return minimum
+double Interval::min() const {
+  return minimum;
+}
+
+// EFFECTS:  Return maximum
+double Interval::max() const {
+  return maximum;
+}
+
+// EFFECTS:  Determine if val is within [minimum, maximum]
+bool Interval::contains(const double val) const {
+  return (minimum <= val) && (val <= maximum);
+}
+
+// EFFECTS:  Determine if val is within (minimum, maximum)
+bool Interval::surrounds(const double val) const {
+  return (minimum < val) && (val < maximum);
+}
+
 const Interval Interval::empty(std::numeric_limits<double>::infinity(),
                                -std::numeric_limits<double>::infinity());
 const Interval Interval::universe(-std::numeric_limits<double>::infinity(),
--- Interval.hpp
+++ Interval.hpp
@@ -6,33 +6,22 @@
 class Interval {
  public:
   // EFFECTS:  Initialize Interval to empty aka (inf, -inf)
-  Interval()
-    : minimum(std::numeric_limits<double>::infinity()),
-      maximum(-std::numeric_limits<double>::infinity()) { }
+  Interval();
 
   // EFFECTS:  Initialize Interval to minimum_in, max_in
-  Interval(const double minimum_in, const double maximum_in)
-    : minimum(minimum_in), maximum(maximum_in) { }
+  Interval(const double minimum_in, const double maximum_in);
 
   // EFFECTS:  Return minimum
-  double min() const {
-    return minimum;
-  }
+  double min() const;
 
   // EFFECTS:  Return maximum
-  double max() const {
-    return maximum;
-  }
+  double max() const;
 
   // EFFECTS:  Determine if val is within [minimum, maximum]
-  bool contains(const double val) const {
-    return (minimum <= val) && (val <= maximum);
-  }
+  bool contains(const double val) const;
 
   // EFFECTS:  Determine if val is within (minimum, maximum)
-  bool surrounds(const double val) const {
-    return (minimum < val) && (val < maximum);
-  }
+  bool surrounds(const double val) const;
 
   static const Interval empty;
   static const Interval universe;
